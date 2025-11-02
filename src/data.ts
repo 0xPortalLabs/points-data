cc63ccb5b12869ae95073ee881c186e266634f7d
Acil Djehiche <acil.djehiche@outlook.com>
2025-11-02 21:12:45 +0000

    format

--- src/data.ts
+++ src/data.ts
@@ -538,7 +538,7 @@ const protocols: Protocol[] = [
   },
   {
     id: 35,
-    name: "hyperflow",
+    name: "Hyperflow",
     listedAt: 1761777901,
     module: "adapters/hyperflow.ts",
     portfolioUrl: "https://hyperflow.fun/points",
@@ -550,18 +550,18 @@ const protocols: Protocol[] = [
       url: "https://hyperflow.fun/"
     }
   },
-    {
-    "id": 36,
-    "name": "Hypurr",
-    "listedAt": 1761937565,
-    "module": "adapters/hypurr.ts",
-    "portfolioUrl": "https://app.hypurr.fi/points",
-    "defillama": {
-      "slug": "hypurrfi-pooled",
-      "description": "Collateralize borrowing positions and loop and lever up your yield",
-      "logo": "https://icons.llama.fi/hypurrfi-pooled.jpg",
-      "twitter": "hypurrfi",
-      "url": "https://hypurr.fi"
+  {
+    id: 36,
+    name: "Hypurr",
+    listedAt: 1761937565,
+    module: "adapters/hypurr.ts",
+    portfolioUrl: "https://app.hypurr.fi/points",
+      defillama: {
+      slug: "hypurrfi-pooled",
+      description: "Collateralize borrowing positions and loop and lever up your yield",
+      logo: "https://icons.llama.fi/hypurrfi-pooled.jpg",
+      twitter: "hypurrfi",
+      url: "https://hypurr.fi"
     }
   }
 ];
