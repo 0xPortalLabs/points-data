fbac08ed130f088749f3fb3876cf1b363b7110c6
Blaze <blaze@dorime.org>
2026-02-18 02:01:41 +0000

    Update data.ts (#37)
    * Update data.ts
    
    * Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -880,7 +880,7 @@ const protocols: Protocol[] = [
       slug: "blockscout",
       description:
         "Blockscout is an open-source blockchain explorer that supports hundreds of EVM networks with transparent transaction, address, and smart contract analytics.",
-      logo: "https://github.com/blockscout/blockscout/blob/master/blockscout.png?raw=1",
+      logo: "https://avatars.githubusercontent.com/u/45625840",
       twitter: "blockscout",
       url: "https://blockscout.com/",
     },
